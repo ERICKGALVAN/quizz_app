552e703449adf34e405054e66aa9bc39d1c7a873
Erick Galvan <galvanerick27@gmail.com>
2024-02-28 11:54:16 -0600

    username

--- lib/home/presentation/screens/home.dart
+++ lib/home/presentation/screens/home.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:quizz_app/auth/presentation/providers/auth_provider.dart';
 import 'package:quizz_app/config/routes/routes.dart';
 import 'package:quizz_app/quizz/presentation/screens/all_quizzes_screen.dart';
 import 'package:quizz_app/quizz/presentation/screens/create_quizz_screen.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  void didChangeDependencies() async {
+    const storage = FlutterSecureStorage();
+    username = await storage.read(key: 'username');
+    setState(() {});
+    super.didChangeDependencies();
+  }
+
+  String? username;
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
     return Scaffold(
       backgroundColor: Colors.white,
       drawer: Drawer(
         child: Column(
           children: [
+            const SizedBox(
+              height: 80,
+            ),
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
+              ).image,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              username ?? 'Nombre',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             const Spacer(),
             TextButton(
               onPressed: () {
